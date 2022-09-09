8e09db1797a780e89135f27dc17527f808b84d50
cre8ivesolutions <tammerz78@gmail.com>
2022-09-09 15:10:33 -0500

    suspense with fallback button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
+
+const AboutUs = React.lazy(() => import('./components/AboutUs'))
+const Packages = React.lazy(() => import('./components/Packages'))
 
 function App() {
+  
   return (
     <div className="App">
+      <Suspense fallback={<div style={{margin:"auto"}}><Button animation="grow"  variant="light" disabled><Spinner as="span" animation="border" size="xl" role="status" aria-hidden="true"/>Loading...</Button></div>}>
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
-      
       <div className="contents">
-          <AboutUs />
-          <Packages />
+             <AboutUs />
+             <Packages />
       </div>
-    </div>
+        </Suspense>
+      </div>
   );
 }
 
