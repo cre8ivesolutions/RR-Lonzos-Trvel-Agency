58be91bad28418b76b00c780671c75e9d4274bb9
cre8ivesolutions <tammerz78@gmail.com>
2022-09-09 14:11:37 -0500

    changed spacing

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
+      
       <div className="contents">
           <AboutUs />
           <Packages />
